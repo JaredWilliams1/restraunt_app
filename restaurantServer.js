1723f65d5f5c2ae062ed9e65b433972f8ada4b3b
Jared Williams <jaredwilliamsemail@gmail.com>
2023-09-26 12:11:35 -0400

    Big documentation update

--- restaurantServer.js
+++ restaurantServer.js
@@ -1,13 +1,30 @@
+/**
+ * @file restaurantServer.js - A Node.js server that handles orders for a restaurant.
+ * @author Jared Williams
+ * @version 1.0.0
+ * @license MIT
+ */
 
+/**
+ * Represents an order placed by a customer.
+ */
 class Order {
     name; 
     tableNum; 
     delivery; 
     items; 
-    info; /* private */
+    info;
     total; 
-    #table; 
+    #table; /* private */
 
+  /**
+   * Creates a new Order object.
+   * @param {String} name - The name of the customer who placed the order.
+   * @param {Number} tableNum - The table number where the customer is seated.
+   * @param {Boolean} delivery - Whether the order is for delivery or dine-in.
+   * @param {Array} items - An array of strings representing the items ordered by the customer.
+   * @param {String} info - Additional information about the order (optional).
+   */
     constructor(name, tableNum, delivery, items, info) {
        this.name = name;
        this.tableNum = tableNum;
@@ -29,6 +46,10 @@ class Order {
       this.#table += `<tr><td>Total Cost</td><td>${this.total.toFixed(2)}</td></tr></table>`;
       
     }
+  /**
+   * Returns an HTML table representing the items ordered and the total cost.
+   * @returns {String} An HTML table representing the items ordered and the total cost.
+   */
     returnTable() {
         return this.#table
     }
@@ -110,7 +131,52 @@ process.stdin.on('readable', () => {
     }
 });
 
-async function insertOrder(newOrder) {
+/**
+ * Handles GET requests to the / endpoint.
+ * Displays the order and admin options (index.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
+app.get("/", (request, response) => { 
+  response.render("index");
+});
+
+/**
+ * Handles GET requests to the /menu endpoint.
+ * Displays the available menu items for orders (displayItems.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
+app.get("/menu", (request, response) => { 
+    const variables = {
+        itemsTable: catalogTable
+      };
+
+    response.render("displayItems", variables);
+});
+
+
+/**
+ * Handles GET requests to the /order endpoint.
+ * Displays the order submission form (placeOrder.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
+app.get("/order", (request, response) => { 
+    const variables = {
+        items: selectMultiple
+      };
+
+    response.render("placeOrder", variables);
+ });
+
+
+/**
+ * Inserts a new order into the database.
+ * @param {Object} newOrder - The order object to be inserted into the database.
+ * @returns {Promise} A Promise that resolves with the ID of the inserted order.
+ */
+ async function insertOrder(newOrder) {
   try {
       const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(newOrder);
       console.log(`Order entry created with id ${result.insertedId}`);
@@ -121,6 +187,50 @@ async function insertOrder(newOrder) {
   }
 }
 
+/**
+ * Handles POST requests to the /order endpoint.
+ * Calls insertOrder to insert a new order into the database.
+ * Displays the order confirmation (orderConfirmation.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
+app.post("/order", (request, response) => { 
+    let {name, tableNum, delivery, itemsSelected, orderInformation} =  request.body;
+    //console.log(name);
+    let orderSubmitted = new Order(name, tableNum, delivery, itemsSelected, orderInformation);
+    const variables = {
+        name: orderSubmitted.name,
+        tableNum: orderSubmitted.tableNum,
+        delivery: orderSubmitted.delivery,
+        orderTable: orderSubmitted.returnTable()
+      };
+    let newOrder = {
+      name: orderSubmitted.name,
+      tableNum: orderSubmitted.tableNum,
+      delivery: orderSubmitted.delivery,
+      total: orderSubmitted.total,
+      orderTable: orderSubmitted.returnTable()
+    };
+    insertOrder(newOrder);
+    response.render("orderConfirmation", variables);
+ });
+
+/**
+ * Handles GET requests to the /reviewOrder endpoint.
+ * Displays the order review form where users enter their name or table number (reviewOrder.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
+ app.get("/reviewOrder", (request, response) => { 
+  response.render("reviewOrder");
+});
+
+/**
+ * Queries the database for an order with the given name or table number.
+ * @param {string} name - The name of the order to be queried.
+ * @param {string} tableNum - The table number of the order to be queried.
+ * @returns {Array} An array of matching orders.
+ */
 async function queryNameOrTableNum(name, tableNum) {
   try {
       await client.connect();
@@ -144,93 +254,13 @@ async function queryNameOrTableNum(name, tableNum) {
   }
 }
 
-async function queryTotal(totalThreshold) {
-  try {
-      await client.connect();
-      let filter = {total: { $gte: Number(totalThreshold)}};
-      console.log(filter);
-      const cursor = await client.db(databaseAndCollection.db)
-                          .collection(databaseAndCollection.collection)
-                          .find(filter);
-      const result = await cursor.toArray();
-      return result;
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
-}
-
-async function removeAll() {
-  try {
-      await client.connect();
-      const result = await client.db(databaseAndCollection.db)
-      .collection(databaseAndCollection.collection)
-      .deleteMany({});
-      return result.deletedCount;
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
-}
-
-app.get("/", (request, response) => { 
-  response.render("index");
-});
-
-//This endpoint displays the displayItems.ejs 
-// template with the table of items available.
-app.get("/menu", (request, response) => { 
-    const variables = {
-        itemsTable: catalogTable
-      };
-
-    response.render("displayItems", variables);
-});
-
-
-//This endpoint displays the placeOrder.ejs 
-// template with the table of items available.
-app.get("/order", (request, response) => { 
-    const variables = {
-        items: selectMultiple
-      };
-
-    response.render("placeOrder", variables);
- });
-
- // This endpoint will process the submission of the placeOrder form, 
-// retrieving the order values and processing the order. Processing an order
-// requires displaying the orderConfirmation.ejs template with a table that 
-// includes the items to be purchased, along with their cost. The last table
-// row has the sum of all the items in the order.
-app.post("/order", (request, response) => { 
-    let {name, tableNum, delivery, itemsSelected, orderInformation} =  request.body;
-    //console.log(name);
-    let orderSubmitted = new Order(name, tableNum, delivery, itemsSelected, orderInformation);
-    const variables = {
-        name: orderSubmitted.name,
-        tableNum: orderSubmitted.tableNum,
-        delivery: orderSubmitted.delivery,
-        orderTable: orderSubmitted.returnTable()
-      };
-    let newOrder = {
-      name: orderSubmitted.name,
-      tableNum: orderSubmitted.tableNum,
-      delivery: orderSubmitted.delivery,
-      total: orderSubmitted.total,
-      orderTable: orderSubmitted.returnTable()
-    };
-    insertOrder(newOrder);
-    response.render("orderConfirmation", variables);
- });
-
-
- app.get("/reviewOrder", (request, response) => { 
-  response.render("reviewOrder");
-});
-
+/**
+ * Handles POST requests to the /reviewOrder endpoint.
+ * Calls queryNameOrTableNum to query the database for an order with the given name or table number.
+ * Displays the order confirmation (orderConfirmation.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
 app.post("/reviewOrder", (request, response) => { 
   (async () => {
     let variables = {}
@@ -255,10 +285,45 @@ app.post("/reviewOrder", (request, response) => {
   })();
 });
 
+/**
+ * Handles GET requests to the /adminTotalQuery endpoint.
+ * Displays the admin total query form (adminTotalQuery.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
 app.get("/adminTotalQuery", (request, response) => { 
   response.render("adminTotalQuery");
 });
 
+/**
+ * Queries the database for orders with a total greater than or equal to the given total threshold.
+ * @param {number} totalThreshold - The total threshold for the query.
+ * @returns {Array} An array of matching orders.
+ */
+async function queryTotal(totalThreshold) {
+  try {
+      await client.connect();
+      let filter = {total: { $gte: Number(totalThreshold)}};
+      console.log(filter);
+      const cursor = await client.db(databaseAndCollection.db)
+                          .collection(databaseAndCollection.collection)
+                          .find(filter);
+      const result = await cursor.toArray();
+      return result;
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+/**
+ * Handles POST requests to the /adminTotalQuery endpoint.
+ * Calls queryTotal to query the database for orders with a total greater than or equal to the given total threshold.
+ * Displays the matching orders (adminTotalDisplay.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
 app.post("/adminTotalQuery", (request, response) => { 
   (async () => {
     let {total} =  request.body;
@@ -275,10 +340,42 @@ app.post("/adminTotalQuery", (request, response) => {
   })();
 });
 
+/**
+ * Handles GET requests to the /adminRemove endpoint.
+ * Displays the form confirming the removal of all entries from the given collection (removeOrders.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
 app.get("/adminRemove", (request, response) => { 
   response.render("removeOrders");
 });
 
+/**
+ * Removes all entries from the given collection.
+ * @returns {number} The number of entries removed.
+ * @throws {Error} If the database connection fails.
+ */
+async function removeAll() {
+  try {
+      await client.connect();
+      const result = await client.db(databaseAndCollection.db)
+      .collection(databaseAndCollection.collection)
+      .deleteMany({});
+      return result.deletedCount;
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+/**
+ * Handles POST requests to the /adminRemove endpoint.
+ * Calls removeAll to remove all entries from the given collection.
+ * Displays the number of entries removed (removalComplete.ejs).
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ */
 app.post("/adminRemove", (request, response) => { 
   (async () => {
     let numberRemoved = await removeAll();
@@ -289,5 +386,4 @@ app.post("/adminRemove", (request, response) => {
   })();
 });
  
-
 app.listen(portNumber);
